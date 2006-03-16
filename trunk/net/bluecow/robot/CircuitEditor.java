810f57cfbc2ba974f0be1316c87cd1a1422a1afb
fuerth <fuerth@671abde3-e930-0410-adf4-93be5ccf8126>
2006-03-16 05:04:25 +0000

    Enabled antialias on circuit editor

--- trunk/net/bluecow/robot/CircuitEditor.java
+++ trunk/net/bluecow/robot/CircuitEditor.java
@@ -12,6 +12,7 @@ import java.awt.Graphics2D;
 import java.awt.LayoutManager;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -424,6 +425,7 @@ public class CircuitEditor extends JPanel {
 
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setColor(getBackground());
 		g2.fillRect(0, 0, getWidth(), getHeight());
 		g2.setColor(getForeground());
